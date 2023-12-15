0e81556ae5f8bfdab037b23f92b8a570492a8a9d
Lutin <tclutin01@gmail.com>
2023-12-15 21:40:23 +0500

    Initial commit

---
+++ HashTables/HashTables/SmallHashTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables.HashTables
+{
+    public class SmallHashTable
+    {
+        public readonly int capacity;
+
+        public readonly List<KeyValuePair<string, string>>[] buckets;
+
+        public SmallHashTable()
+        {
+            capacity = 1000;
+            buckets = new List<KeyValuePair<string, string>>[capacity];
+        }
+    }
+}
---
+++ HashTables/Program.cs
@@ -0,0 +1,16 @@
+﻿using HashTables.HashTables;
+
+namespace HashTables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SmallHashTable table = new SmallHashTable();
+
+            Console.WriteLine(table.capacity);
+        }
+    }
+}
